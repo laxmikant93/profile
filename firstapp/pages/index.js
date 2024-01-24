9e44d2900bd01f1bcb10a28c837b95a3cf03e20a
laxmikant swain <laxmikant@edneed.com>
2024-01-25 02:32:52 +0530

    added new next

---
+++ firstapp/pages/index.js
@@ -0,0 +1,30 @@
+function Detail2() {
+  return (
+    <div>
+    <h3>laxmikantswain93@gmail.com</h3>
+    <Detail3 />
+    </div>
+  )
+}
+function Detail() {
+  return (
+    <>
+    <h2>9178628382</h2>
+    <Detail2 />
+    </>
+  )
+}
+function Detail3() {
+  return (
+    <>
+    <h4>Plot 1369 7621 Satyavihar bhubaneswar 10</h4></>
+)}
+function Home() {
+  return (
+  <>
+   <h1>Laxmikant Swain</h1>
+   <Detail />
+  </>
+  )
+}
+export default Home;
---
+++ firstapp/pages/portfolio.js
@@ -0,0 +1,9 @@
+
+
+
+function portfolio() {
+    return (
+        <h1>my portfolio</h1>
+    )
+}
+export default portfolio;
