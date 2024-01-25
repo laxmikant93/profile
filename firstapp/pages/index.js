4e3aa67e83f25dd1309fd62c602b22993f194e0b
laxmikant swain <laxmikant@edneed.com>
2024-01-26 03:43:48 +0530

    add

--- firstapp/pages/index.js
+++ firstapp/pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 function Detail2() {
   return (
     <div>
@@ -20,9 +21,20 @@ function Detail3() {
     <h4>Plot 1369 7621 Satyavihar bhubaneswar 10</h4></>
 )}
 function Home() {
+const [value,setvalue] = useState(10);
+const incrementvalue = () => {
+  setvalue(value + 1);
+  console.log(setvalue)
+}
+const decrementvalue = () => {
+  setvalue(value - 1);
+}
   return (
   <>
-   <h1>Laxmikant Swain</h1>
+     <h1>Laxmikant Swain</h1>
+     Currentvalue: <p>{value}</p>
+     <button onClick={incrementvalue}>+</button>
+     <button onClick={decrementvalue}>-</button>
    <Detail />
   </>
   )
